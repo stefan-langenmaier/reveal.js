cabc00f7c0b4b2dce28f31bbc376ff4ec19b20a2
Stefan Langenmaier <stefan.langenmaier@makandra.de>
2020-03-02 08:52:43 +0100

    moved portage-configroot

--- setup/docker/start-builder.sh
+++ setup/docker/start-builder.sh
@@ -7,7 +7,8 @@ docker run \
 	-v /home/stefan/.gentoo/portage:/var/db/repos/gentoo \
 	-v /home/stefan/.gentoo/packages:/var/cache/binpkgs \
 	-v /home/stefan/.gentoo/distfiles:/var/cache/distfiles \
-	-v /home/stefan/.gentoo/build-root:/var/build-root \
+	-v /home/stefan/.gentoo/build-root/rootfs:/var/build-root/rootfs \
+	-v /home/stefan/git/docker-composability-presentation/setup/docker/portage-configroot:/var/build-root/portage-configroot \
 	-v /home/stefan/.gentoo/container-profiles:/usr/local/portage/container-profiles \
 	-v /home/stefan/.gentoo/config/repos.conf/container-profiles.conf:/etc/portage/repos.conf/container-profiles.conf \
 	gentoo-builder \
