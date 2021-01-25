e7bbefded3f58ae7232b6d6c8655e31f28d6c5f6
Stefan Langenmaier <stefan.langenmaier@makandra.de>
2021-01-25 21:13:42 +0100

    cleanup commands and remove unncessary slides

--- setup/docker/prepare-builder.sh
+++ setup/docker/prepare-builder.sh
@@ -5,3 +5,8 @@ docker cp host-portage/package.accept_keywords gentoo-builder:/etc/portage/
 
 docker exec -i gentoo-builder emerge webapp-config eselect-php sys-libs/libcap
 docker exec -i gentoo-builder emerge -O nextcloud
+
+
+docker exec -i gentoo-builder mkdir -p /var/build-root/rootfs/usr/share/webapps
+docker exec -i gentoo-builder ln -sf /usr/share/webapps/nextcloud /var/build-root/rootfs/usr/share/webapps/nextcloud
+docker exec -i gentoo-builder mkdir /var/build-root/rootfs/var/db/webapps
