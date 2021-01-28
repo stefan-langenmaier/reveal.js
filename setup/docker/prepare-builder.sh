e607fa0972de32b068e2618408386ab260fcf9a6
Stefan Langenmaier <stefan.langenmaier@makandra.de>
2021-01-28 18:12:26 +0100

    last minute changes

--- setup/docker/prepare-builder.sh
+++ setup/docker/prepare-builder.sh
@@ -7,6 +7,7 @@ docker exec -i gentoo-builder emerge webapp-config eselect-php sys-libs/libcap
 docker exec -i gentoo-builder emerge -O nextcloud
 
 
+docker exec -i gentoo-builder bash -c "rm /var/build-root/rootfs/* -R" # to avoid an early expansion
 docker exec -i gentoo-builder mkdir -p /var/build-root/rootfs/usr/share/webapps
 docker exec -i gentoo-builder ln -sf /usr/share/webapps/nextcloud /var/build-root/rootfs/usr/share/webapps/nextcloud
-docker exec -i gentoo-builder mkdir /var/build-root/rootfs/var/db/webapps
+docker exec -i gentoo-builder mkdir -p /var/build-root/rootfs/var/db/webapps
